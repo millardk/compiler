0d5b52b5bf2d55d5ca18aaed2541c05598b9769f
gamerpuppy <keegan.millard@gmail.com>
2018-02-23 16:45:49 -0700

    added test

--- src/Driver.java
+++ src/Driver.java
@@ -1,43 +1,44 @@
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+public class Driver implements ParseTreeListener{
 
-public class Driver{
+    boolean error = false;
 
     public static void main(String[] args) throws Exception {
 
-        Map <Integer,String> typeMap = makeTypenameMap();
-
-        String testcase = "./tests/test2.little";
+        String testcase = "./test2s/test.little";
         CharStream cs = CharStreams.fromFileName(testcase);
         LittleLexer lexer = new LittleLexer(cs);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         tokens.fill();
 
-        for(int i = 0; i < tokens.size()-1; i++){
-            Token tok = tokens.get(i);
-            System.out.println("Token Type: " + typeMap.get(tok.getType()));
-            System.out.println("Value: " + tok.getText());
-
-        }
-    }
-
-    private static Map<Integer, String> makeTypenameMap(){
-        HashMap<Integer,String> ret = new HashMap<>();
+        LittleParser parser = new LittleParser(tokens);
         try {
-            Scanner in = new Scanner(new File("./gen/LittleLexer.tokens"));
-            while (in.hasNextLine()){
-                String[] line = in.nextLine().split("=");
-                ret.put(Integer.valueOf(line[1]),line[0]);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            parser.program();
+        } catch(Exception e){
         }
-        return ret;
+
     }
 
+
+    @Override
+    public void visitTerminal(TerminalNode terminalNode) {
+
+    }
+
+    @Override
+    public void visitErrorNode(ErrorNode errorNode) {
+        error = true;
+    }
+
+    @Override
+    public void enterEveryRule(ParserRuleContext parserRuleContext) {
+
+    }
+
+    @Override
+    public void exitEveryRule(ParserRuleContext parserRuleContext) {
+
+    }
 }
